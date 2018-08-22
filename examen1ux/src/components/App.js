9fcdbda975dc605f224babc20b846df089e76e7c
ilich-garcia <ilich-garcia@unitec.edu>
2018-08-22 16:31:14 -0600

    Esta papada...

--- examen1ux/src/components/App.js
+++ examen1ux/src/components/App.js
@@ -16,6 +16,7 @@ class App extends Component {
       users: [],
       posts: [],
       visible: "Público",
+      filter: "Todos",
       temp_CurrentPost: null,
       commentModal: " "
     };
@@ -26,9 +27,11 @@ class App extends Component {
     this.handlePost = this.handlePost.bind(this);
     this.handleFollow = this.handleFollow.bind(this);
     this.handleVisible = this.handleVisible.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
     this.handleComment = this.handleComment.bind(this)
     this.handleCommentModal = this.handleCommentModal.bind(this)
     this.handleLike = this.handleLike.bind(this)
+    this.handleAlert = this.handleAlert.bind(this)
   }
 
 
@@ -45,8 +48,10 @@ class App extends Component {
 
   componentWillMount() {
     firebase.auth().onAuthStateChanged(user => { // Cada vez que nos loggeemos o nos salgamos, el user tendrá información.
-      this.setState({ user });
-      firebase.database().ref('users/' + user.uid).set({ displayName: user.displayName, photoURL: user.photoURL, followers: [] });
+      if (user !== null) {
+        this.setState({ user });
+        firebase.database().ref('users/' + user.uid).set({ displayName: user.displayName, photoURL: user.photoURL, followers: [] });
+      }
     });
 
     firebase.database().ref('pictures').on('child_added', snapshot => {
@@ -60,16 +65,44 @@ class App extends Component {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
-      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
-      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+      .then(result => {
+        console.log(`${result.user.email} ha iniciado sesión`)
+        this.setState(() => ({
+          alert: <div className="alert alert-success alert-dismissible fade show">
+            <button onClick={this.handleAlert} type="button" className="close" data-dismiss="alert">&times;</button>
+            <strong>¡Has iniciado sesión exitosamente!</strong>
+          </div>
+        }))
+      })
+      .catch(error => {
+        console.log(`Error ${error.code}: ${error.message}`)
+        this.setState(() => ({
+          alert: <div className="alert alert-danger alert-dismissible fade show">
+            <button onClick={this.handleAlert} type="button" className="close" data-dismiss="alert">&times;</button>
+            <strong>¡No se ha podido iniciar sesión!</strong>
+          </div>
+        }))
+      });
 
     //let user = {}
     //const dbRef = firebase.database().ref('users').dbRef.push().set(user);
 
   }
 
+  handleAlert() {
+    this.setState(() => ({
+      alert: null
+    }))
+  }
+
   handleLogOut() {
-    firebase.auth().signOut()
+    this.setState({
+      alert: <div className="alert alert-success alert-dismissible fade show">
+        <button onClick={this.handleAlert} type="button" className="close" data-dismiss="alert">&times;</button>
+        <strong>¡Has cerrado sesión exitosamente!</strong>
+      </div>
+    })
+    firebase.auth().signOut();
   }
 
   handleUpload(event) {
@@ -154,16 +187,17 @@ class App extends Component {
         document.getElementById("Ptopic").value = ""
         document.getElementById("Pmessage").value = ""
         this.setState(() => ({
-          visible: "Público"
+          visible: "Público",
+          alert: <div className="alert alert-success alert-dismissible fade show">
+            <button onClick={this.handleAlert} type="button" className="close" data-dismiss="alert">&times;</button>
+            <strong>¡Has posteado exitosamente!</strong>
+          </div>
         }))
       } else {
         alert("Llene cada campo. :v")
       }
   }
 
-  handleFollow() {
-    alert("Follow")
-  }
 
   handleVisible() {
     if (this.state.visible === "Público") {
@@ -177,6 +211,22 @@ class App extends Component {
     } else {
       this.setState((e) => ({
         visible: "Público"
+      }))
+    }
+
+  }
+  handleFilter() {
+    if (this.state.filter === "Todos") {
+      this.setState((e) => ({
+        filter: "Mis privados"
+      }))
+    } else if (this.state.filter === "Mis privados") {
+      this.setState((e) => ({
+        filter: "Sólo seguidores"
+      }))
+    } else {
+      this.setState((e) => ({
+        filter: "Todos"
       }))
     }
   }
@@ -202,9 +252,9 @@ class App extends Component {
 
   handleLike() {
     if (firebase.database().ref('posts/' + localStorage.getItem("idPost") + "/likes"))
-      firebase.database().ref('posts/' + localStorage.getItem("idPost") + "/likes/" + this.state.user.uid).update({uid:this.state.user.uid})
-      else
-      firebase.database().ref('posts/' + localStorage.getItem("idPost") + "/likes/" + this.state.user.uid).set({uid:this.state.user.uid})
+      firebase.database().ref('posts/' + localStorage.getItem("idPost") + "/likes/" + this.state.user.uid).update({ uid: this.state.user.uid })
+    else
+      firebase.database().ref('posts/' + localStorage.getItem("idPost") + "/likes/" + this.state.user.uid).set({ uid: this.state.user.uid })
   }
 
   handleComment() {
@@ -229,87 +279,162 @@ class App extends Component {
       })
   }
 
-  render() {
-    const posts = this.state.posts.map(newVariable => {
-      if (newVariable.val().type === "Público" ||
-        newVariable.val().type === "Privado" && newVariable.val().uid === this.state.user.uid) {
-        let comments = []
-        if (newVariable.val().comments) {
-          Object.keys(newVariable.val().comments).map(c => {
-            firebase.database().ref('posts/' + newVariable.key + "/comments/" + c).on("value", (snapshot) => {
-              if (snapshot.val() !== null) {
-                comments.push(
-                  <div key={newVariable.key + snapshot.key} className="card">
-                    <div className="comm">
-                      <img style={{ width: 70 + "%", marginRight: 1 + "% !important" }} className="rounded mx-auto grid-i" src={snapshot.val().upic} alt="User-pic" />
-                      <div className="grid-i">
-                        <h4 style={{ textAlign: "left !important", fontSize: 0.8 + "rem" }} className="card-title">{snapshot.val().uname}</h4>
-                      </div>
-                    </div>
-                    <p className="card-text margin1">{snapshot.val().text}</p>
-                  </div>
-                )
-              }
-            })
-          })
-        }
-        return (
-          <div key={newVariable.key} id={newVariable.key} className="card">
-
-            <div className="card-header">
-              <img style={{ width: 90 + "%", marginRight: 1 + "% !important" }} className="img-thumbnail mx-auto grid-i" src={newVariable.val().upic} alt="User-pic" />
-              <div className="grid-i">
-                <h4 style={{ textAlign: "left !important", fontSize: 1 + "rem" }} className="card-title">{newVariable.val().uname}</h4>
-                <h6 style={{ fontSize: 0.7 + "rem", color: "grey" }} className="text-left">{newVariable.val().date}</h6>
-              </div>
-              <h3 className="card-title grid-i">{newVariable.val().topic}</h3>
-            </div>
-            <div className="card-body">
-              <p className="card-text">{newVariable.val().message}</p>
-            </div>
-            <div className="text-right" style={{ marginRight: 1 + "%", marginBottom: 1 + "%" }}>
-              {this.state.user ?
-                <button className="btn btn-info" type="button" onClick={this.handleFollow}>Seguir Autor</button>
-                : ""}
-              <button id={newVariable.key} type="button" className="btn btn-outline-secondary dropdown-toggle buttonPost" data-toggle="dropdown" onClick={this.handleCommentModal}>Comentarios <small>{newVariable.val().comments ? Object.keys(newVariable.val().comments).length : 0}</small></button>
-
-              <div className={localStorage.getItem("idPost") === newVariable.key ? "dropdown-menu dropdown-menu-right " + this.state.CommentModal : "dropdown-menu dropdown-menu-right "} style={{ marginRight: 1 + "%", marginBottom: 1 + "%" }}>
-                <div className="form-group card">
-                  {comments}
-                  {this.state.user ? <div>
-                    <textarea className="form-control" rows="1" id={"comment" + newVariable.key} type="text" placeholder="Comentario..." />
-                    <button className="btn btn-dark " type="button" onClick={this.handleComment}>Comentar</button>
-                  </div>
-                    : ""}
-                </div>
-              </div>
-              {this.state.user ?
-                <button type="button" id={newVariable.key} className="btn btn-primary buttonPost" onClick={this.handleLike}>Me gusta <small>{newVariable.val().likes ? Object.keys(newVariable.val().likes).length : 0}</small></button>
-                : ""}
-            </div>
-
+  handleFollow() {
+    firebase.database().ref('posts/' + localStorage.getItem("idPost") + "").on("value", (snapshot) => {
+      if (snapshot.val() !== null) {
+        firebase.database().ref('users/' + snapshot.val().uid + "/followers/" + this.state.user.uid).update({ uid: this.state.user.uid })
+        this.setState({
+          alert: <div className="alert alert-info alert-dismissible fade show">
+            <button onClick={this.handleAlert} type="button" className="close" data-dismiss="alert">&times;</button>
+            <strong>{"¡Estas siguiendo al autor este post!"}</strong>
           </div>
-        )
+        })
+
       }
     })
+  }
+
+  renderPost(newVariable) {
+    let comments = []
+    if (newVariable.val().comments) {
+      Object.keys(newVariable.val().comments).map(c => {
+        return firebase.database().ref('posts/' + newVariable.key + "/comments/" + c).orderByKey().on("value", (snapshot) => {
+          if (snapshot.val() !== null) {
+            comments.reverse().push(
+              <div key={newVariable.key + snapshot.key} style={{ paddingTop: 1 + "%" }} className="comment_ margin1">
+                <div className="comm">
+                  <img style={{ width: 70 + "%", marginRight: 1 + "% !important", marginTop: 4 + "% !important" }} className="rounded mx-auto grid-i" src={snapshot.val().upic} alt="User-pic" />
+                  <div className="grid-i">
+                    <h4 style={{ textAlign: "left !important", fontSize: 0.8 + "rem", margin: 1 + "% !important" }} className="card-title">{snapshot.val().uname}</h4>
+                  </div>
+                </div>
+                <p style={{ marginLeft: 1 + "%" }} className="card-text ">{snapshot.val().text}</p>
+              </div>
+            )
+          }
+        })
+      })
+    }
+    return (
+      <div key={newVariable.key} id={newVariable.key} className="card">
+
+        <div className="card-header">
+          <img style={{ width: 90 + "%", marginRight: 1 + "% !important" }} className="img-thumbnail mx-auto grid-i" src={newVariable.val().upic} alt="User-pic" />
+          <div className="grid-i">
+            <h4 style={{ textAlign: "left !important", fontSize: 1 + "rem" }} className="card-title">{newVariable.val().uname}</h4>
+            <h6 style={{ fontSize: 0.7 + "rem", color: "grey" }} className="text-left">{newVariable.val().date}</h6>
+          </div>
+          <h3 className="card-title grid-i">{newVariable.val().topic}</h3>
+        </div>
+        <div className="card-body">
+          <p className="card-text">{newVariable.val().message}</p>
+        </div>
+        <div className="text-right" style={{ marginRight: 1 + "%", marginBottom: 1 + "%" }}>
+          {this.state.user ?
+            <button id={newVariable.key} className="btn btn-info buttonPost" type="button" onClick={this.handleFollow}>Seguir Autor</button>
+            : ""}
+          <button id={newVariable.key} type="button" className="btn btn-outline-secondary dropdown-toggle buttonPost" data-toggle="dropdown" onClick={this.handleCommentModal}>Comentarios <small>{newVariable.val().comments ? Object.keys(newVariable.val().comments).length : 0}</small></button>
+
+          <div className={localStorage.getItem("idPost") === newVariable.key ? "dropdown-menu dropdown-menu-right " + this.state.CommentModal : "dropdown-menu dropdown-menu-right "} style={{ marginRight: 1 + "%", marginBottom: 1 + "%" }}>
+            <div className="form-group margin1">
+              {comments}
+              {this.state.user ? <div>
+                <textarea className="form-control" rows="1" id={"comment" + newVariable.key} type="text" placeholder="Comentario..." />
+                <button className="btn btn-dark " type="button" onClick={this.handleComment}>Comentar</button>
+              </div>
+                : ""}
+            </div>
+          </div>
+          {this.state.user ?
+            <button type="button" id={newVariable.key} className="btn btn-primary buttonPost" onClick={this.handleLike}>Me gusta <small>{newVariable.val().likes ? Object.keys(newVariable.val().likes).length : 0}</small></button>
+            : ""}
+        </div>
+
+      </div>
+    )
+  }
+
+  render() {
+    if (this.state.user)
+      localStorage.setItem("CurrentUser", this.state.user.uid)
+
+    const posts = this.state.posts.map(newVariable => {
+
+      let filterFlag = false
+      if (this.state.filter === "Todos") {
+        if (this.state.user) {
+          if ((newVariable.val().type === "Público") || (newVariable.val().type === "Privado" && newVariable.val().uid === this.state.user.uid)) {
+            filterFlag = true
+          } else if (newVariable.val().type === "Sólo seguidores") {
+            firebase.database().ref("users/" + newVariable.val().uid).once("value").then(function (snapshot) {
+              if (snapshot.child("followers/" + localStorage.getItem("CurrentUser")).exists()) {
+
+                 this.renderPost(newVariable)
+              }
+            })
+          }
+        }
+        else if (newVariable.val().type === "Público") {
+          filterFlag = true
+        }
+      } else if (this.state.filter === "Mis privados") {
+        if (this.state.user) {
+          if (newVariable.val().type === "Privado" && newVariable.val().uid === this.state.user.uid) {
+            filterFlag = true
+          }
+        }
+      } else if (newVariable.val().type === "Sólo seguidores") {
+        if (this.state.user) {
+          firebase.database().ref("users/" + newVariable.val().uid).once("value").then(function (snapshot) {
+            if (snapshot.child("followers/" + localStorage.getItem("CurrentUser")).exists()) {
+               this.renderPost(newVariable)
+            }
+          })
+        }
+      }
+      if (filterFlag) {
+        return this.renderPost(newVariable)
+      } else {
+        return (null)
+      }
+    }).reverse()
 
     return (
-      <div className="App">
+      <div id="App" className="App">
+        {/*
         <header className="App-header">
           <h1 className="App-title">Mi Proyecto</h1>
         </header>
-        {this.state.user ?
-          <div>
-            <div className="form-group card">
-              <input className="form-control" id="Ptopic" type="text" placeholder="Tema" />
-              <textarea className="form-control" rows="5" id="Pmessage" type="text" placeholder="Cuerpo de la publicación..." />
+        */}
+        <nav className="navbar navbar-expand-md bg-dark navbar-dark">
+          <a className="navbar-brand" href="#App">Así como estaba :v</a>
+          {this.state.user ?
+            <div type="button" className="logoutButton" onClick={this.handleLogOut}>
+              <img style={{ width: 7 + "vmin" }} className="rounded mx-auto" src={this.state.user.photoURL} alt="User-pic" />
+              <p style={{ fontSize: 1 + "vim", color: "white" }}>¡Hola, {this.state.user.displayName}!</p>
             </div>
-            <button className="btn btn-outline-dark" style={{ marginRight: 1 + "%" }} type="button" onClick={this.handleVisible}>{this.state.visible}</button>
-            <button className="btn btn-success" type="button" onClick={this.handlePost}>Postear</button>
+            :
+            <button className="btn btn-primary loginButton" type="button" onClick={this.handleAuth}>Iniciar sesión</button>
+          }
+        </nav>
+        <div id="Desk">
+          {this.state.alert ? this.state.alert : ''}
+          {this.state.user ?
+            <div>
+              <div style={{ marginRight: 5 + "%", marginLeft: 5 + "%", marginTop: 2.5 + "%", marginBottom: 0 + "%", paddingBottom: 5 + "%" }} className="form-group">
+                <input className="form-control" id="Ptopic" type="text" placeholder="Tema" />
+                <textarea className="form-control" rows="5" id="Pmessage" type="text" placeholder="Cuerpo de la publicación..." />
+              </div>
+              <button className="btn btn-outline-dark" type="button" onClick={this.handleVisible}>{this.state.visible}</button>
+              <button className="btn btn-success" type="button" onClick={this.handlePost}>Postear</button>
+            </div>
+            : ""}
+          <div id="board" className="card">
+            <button className="btn btn-dark" type="button" onClick={this.handleFilter}>{this.state.filter}</button>
+            {posts}
           </div>
-          : ""}
-        <div id="board" className="card">{posts}</div>
-        {this.renderLogginButton()}
+          {/*this.renderLogginButton()*/}
+        </div>
       </div>
     );
   }
